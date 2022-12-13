a9381db5324ce6f30389dfe0c3cb7185c9dcfef5
Mason <masonsepulveda@gmail.com>
2022-12-13 17:33:20 -0600

    added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -57,17 +57,15 @@ const Portfolio = () => {
                 <p className="py-6">Check out some of my work right here</p>
             </div>
 
-            
-
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
 
             {
                 portfolios.map(({id, src, site, code}) => (
                     <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                         <img src={src} alt=""  className="rounded-md duration-200 hover:scale-105" />
-                        <div className="flex flex-center justify-center ">
+                        <div className="flex items-center justify-center ">
                             <button href="" target="_blank" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                                <a href={site} target="_blank" >
+                                <a href={site} rel="noreferrer" target="_blank" >
                                     Site
                                 </a>
                             </button>
